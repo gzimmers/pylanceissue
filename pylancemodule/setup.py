f4c808d657953c90f292d2a8b3f3f3f562b5339d
Gregory Zimmers <zimmersgreg@gmail.com>
2021-03-17 20:56:56 -0600

    Example issue

---
+++ pylancemodule/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pylancemodule',
+    version="0.0.1",
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3.8',
+)
---
+++ setup.sh
@@ -0,0 +1,2 @@
+cd ./pylancemodule
+pip install -e .
